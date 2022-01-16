047537d28a6d4b0594fc20b11a4fc75142e12225
yusuf bestas <bestasyusuf8@gmail.com>
2022-01-16 14:23:35 +0300

    AddTwoNumbers https://leetcode.com/problems/add-two-numbers/

---
+++ src/main/java/com/example/algoritma/AddTwoNumbers.java
@@ -0,0 +1,73 @@
+package com.example.algoritma;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ *
+ */
+public class AddTwoNumbers {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode result = new ListNode(0);
+        collectListItems(l1, l2, result, carry);
+        return result.next;
+    }
+
+    public static ListNode collectListItems(ListNode l1, ListNode l2, ListNode result, int carry) {
+        if (l1 == null && l2 == null) {
+            if (carry > 0) {
+                result.next = new ListNode(carry);
+            }
+            return result;
+        }
+        int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+        carry = sum / 10;
+        result.next = new ListNode(sum % 10);
+        return collectListItems(l1 != null ? l1.next : null, l2 != null ? l2.next : null, result.next, carry);
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(9);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
+        l1.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next = new ListNode(9);
+        l1.next.next.next.next.next.next = new ListNode(9);
+
+        ListNode l2 = new ListNode(9);
+        l2.next = new ListNode(9);
+        l2.next.next = new ListNode(9);
+        l2.next.next.next = new ListNode(9);
+
+        addTwoNumbers(l1, l2);
+    }
+}
